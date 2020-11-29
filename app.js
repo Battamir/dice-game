439f741a9511e70483521b3c892ba181982a40ea
tamiraa <battamir941114@gmail.com>
2020-11-30 06:27:34 +0800

    shoog hayhad zurag soligddog bolgoson

--- app.js
+++ app.js
@@ -16,15 +16,22 @@ var currentScore = 0;
 
 
 // Шоо аль талаараа буухыг хадгалах хувьсагч  
-
-var dice = Math.floor( Math.random( ) * 6 ) + 1;
-
-document.querySelector('#score-0').textContent = dice;
-
-console.log('шоо' + dice);
+// getElementByid idgaar haihad system hurdtai bolno
+// anonymous function ashiglaj 1 ajil hiih function bichsen
+// callback function ashiglaw
+// shoog hayhad zurag uurchlugddug bogoson
 
 
-document.querySelector('#score-0').textContent = '0';
-document.querySelector('#score-1').innerHTML = '<em>Yes!<em>';
-document.querySelector('#current-0').textContent = '0';
-document.querySelector('#current-1').textContent = '0';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+var diceHayah = document.querySelector('.dice');
+diceHayah.style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click' , function() {
+    var diceNumber = Math.floor( Math.random( ) * 6 ) + 1;
+    diceHayah.style.display = 'block';
+    diceHayah.src = 'dice-' + diceNumber + '.png';
+} );
